9902a4c28f64f7bc938ee74e80e21e5c92924263
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-09 00:54:17 +0200

    add ID

--- src/client.cpp
+++ src/client.cpp
@@ -6,7 +6,7 @@ int main() {
     std::string host = "192.168.1.135";
     int port = 8181;
 
-    socket.connect(host, port);
+    socket.connect(host, port, "RPI");
     std::usleep(1000000);
 
     socket.disconnect();
