e90920532bd91ddc17563583a7fae22dc438572e
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-09 00:31:28 +0200

    host

--- src/host.cpp
+++ src/host.cpp
@@ -6,13 +6,7 @@ int main() {
     int port = 8181;
     int backlog = 5;
 
-    // Host a server
     socket.host(port, backlog);
-    std::cout << "Server is hosting on port " << port << " with a backlog of " << backlog << std::endl;
-
-    // Close the server socket
-    socket.close();
-    std::cout << "Server socket closed." << std::endl;
 
     return 0;
 }
