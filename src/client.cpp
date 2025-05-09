441816bc0bdf2a2f983e452e55a030ff6d1c8ee0
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-09 14:37:51 +0200

    test dingen

--- src/client.cpp
+++ src/client.cpp
@@ -6,17 +6,18 @@ int main() {
     std::string host = "192.168.1.135";
     int port = 8181;
 
-    socket.connect(host, port, "RPI");
+    socket.connect(host, port, "RPIB");
 
     while (true) {
         std::string message = socket.pollServer();
 
         sleep(1);
         socket.sendToServer("test");
+        cout << "Sent message to server: test" << std::endl;
         
     }
 
-
+    cout << "pp" << endl;
     socket.disconnect();
 
     return 0;
