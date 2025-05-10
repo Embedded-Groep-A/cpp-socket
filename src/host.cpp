e5acb5f9fd51a9abc3e7bec391a1c0b5e2a6ed7a
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-10 14:12:28 +0200

    test getallen

--- src/host.cpp
+++ src/host.cpp
@@ -9,13 +9,14 @@ int main() {
     socket.host(port, backlog);
 
     while(true) {
+        std::cout << "1" << std::endl;
         socket.accept();
         auto [clientID, message] = socket.poll();
-
+        std::cout << "5" << std::endl;
         if (clientID.empty() || message.empty()) {
             continue;
         }
-
+        std::cout << "6" << std::endl;
         if (message == "test") {
             socket.sendToClient(clientID, "testACK");
         }
