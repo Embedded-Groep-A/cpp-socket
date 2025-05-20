5547411cd06a65207efb6556749e05f58077cd4e
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-20 10:17:45 +0200

    poll test

--- src/rpia.cpp
+++ src/rpia.cpp
@@ -2,7 +2,18 @@
 #include "PiBus.h"
 #include "Message.h"
 #include <iostream>
-#include <unistd.h> 
+#include <unistd.h>
+
+typedef struct {
+    uint8_t uid[4];
+    const char* eigenaarNaam;
+} Eigenaar;
+
+Eigenaar eigenaars[] = {
+    {{0x77, 0xDA, 0xAC, 0x02}, "Cas"},
+    {{0xB1, 0xFF, 0x74, 0x1D}, "Thijs"},
+   // {{0x64, 0x81, 0xE6, 0x03}, "Ahmed"}
+};
 
 int main() {
     Socket socket;
@@ -14,10 +25,12 @@ int main() {
     piBus.openSerial("/dev/ttyS0", 115200);
 
     while (true) {
-        piBus.send(MessageType::ACCEPT, "AHMED");
-        sleep(6);
-        piBus.send(MessageType::REJECT, "");
-        sleep(6);
+        auto [type, data] = piBus.poll();
+        if (type == MessageType::UNKNOWN) {
+            std::cerr << "niks" << std::endl;
+            continue;
+        }
+        std::cout << "Received from bus: " << typeToString(type) << data << std::endl;
     }
 
         
